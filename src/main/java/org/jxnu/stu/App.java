ae3477dd8e39e9609bf9793773a17adea3e145df
shaohengwei <296017678@qq.com>
2019-03-30 15:58:59 +0800

    inin project

---
+++ src/main/java/org/jxnu/stu/App.java
@@ -0,0 +1,13 @@
+package org.jxnu.stu;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
